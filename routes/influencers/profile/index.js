0535c5f9c94f281cf833533edcda6671f2ce1461
harsh gupta <theharshgupta2020@gmail.com>
2023-07-18 02:08:50 +0530

    change

--- routes/influencers/profile/index.js
+++ routes/influencers/profile/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const {
+    updatedInfluencerPrice,
+    add_influencer_price
+} = require('../../../controllers/influencer/profile/influencer_price.controller');
 const {
     GetContentNiche,
     AddContentNiche
@@ -16,6 +19,16 @@ router.get("/get-content-niche",authJWT,(req, res)=> {
     
     router.post("/add-content-niche",authJWT,(req, res)=> {
         AddContentNiche(req, res);
+       console.log("pk");
+    });
+
+    router.patch("/update-influencer-price", authJWT, (req, res) => {
+        updatedInfluencerPrice(req, res);
+    });
+
+    router.post("/add-influencer-price", authJWT, (req, res) => {
+       // add_influencer_price(req, res);
+       console.log("pk");
     });
 
 //}
