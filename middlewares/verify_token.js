35e4547536cb4992d4370b8731af377e58457083
harsh gupta <theharshgupta2020@gmail.com>
2023-07-20 01:58:32 +0530

    token verify

--- middlewares/verify_token.js
+++ middlewares/verify_token.js
@@ -1,71 +1,87 @@
 const tableNames = require("../utils/table_name");
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 
+async function authJWT(req, res, next) {
+  try {
+    var authorization = req.headers.authorization;
+    token = authorization.split(" ")[1];
 
-async function authJWT  (req, res, next){
+    if (token == null) {
+      res.status(404).send({ message: "Token not found" });
+    } else {
+      const privatekey = process.env.privateKey;
+      jwt.verify(token, privatekey, async (err, decoded) => {
+        if (err) {
+          res.status(200).send({ message: "invalid token" });
+        }
+        data = decoded;
+        console.log(data.brandlog);
 
-    try {
-        var authorization = req.headers.authorization;
-        token = authorization.split(' ')[1];
- 
-       if(token == null){
-        res.status(404).send({message:"Token not found"})
-    
-    }else{
-        const privatekey =  process.env.privateKey;
-         jwt.verify(token,privatekey, async (err, decoded) => {
-            if(err) {
-                res.status(200).send({message:"invalid token"})
+        if (data.brandlog == true) {
+          let Sqltoken = await tableNames.gen_token.findOne({
+            where: { gen_token: token, brand_id: data.brands_id },
+          });
+          if (!Sqltoken) {
+            res.status(403).send({ message: "token failed" });
+          } 
+        else {
+            let Sqlquery = await tableNames.brands.findOne({
+              where: { brands_id: data.brands_id },
+            });
+            if (!Sqlquery) {
+              res.status(403).send({ message: "brand  not found" });
             }
-                data = decoded;
-                console.log(data.brandlog);
+         }
+        } else {
+          let Sqltoken = await tableNames.gen_token.findOne({
+            where: { gen_token: token, influencer_id: data.influencer_id },
+          });
+          if (!Sqltoken) {
+            res.status(403).send({ message: "token failed" });
+          } else {
+            let Sqlquery = await tableNames.influencer_users.findOne({
+              where: { influencer_id: data.influencer_id },
+            });
+            if (!Sqlquery) {
+              res.status(403).send({ message: "Influencer not found" });
+            }
+          }
+        }
 
-                if(data.brandlog == true){
-                    let Sqlquery = await tableNames.brands.findOne({ where: { brands_id: data.brands_id,} });     
-                 if(!Sqlquery)
-                    { res.status(403).send({message:"brand  not found"})}
-                }else{
-                    let Sqlquery = await tableNames.influencer_users.findOne({ where: { influencer_id: data.influencer_id,} });     
-                    if(!Sqlquery)
-                       { res.status(403).send({message:"Influencer not found"})}
-                }
-                
-               
-                    res.header("Access-Control-Allow-Origin", "*");
-                    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-                    res.header("Access-Control-Allow-Methods", "PUT, GET,POST");
-                    next();  
-        })
-    //    let Sqlquery = await tableNames.gen_token.findOne({ where: { gen_token: token,} });
-    //   console.log(Sqlquery);
-    //    if(Sqlquery != null)
-    //    {
-    //     //       if(!token){
-    //     //     res.status(404).send({message:"Token not found"})    
-    //     // }else{
-    //     //        token = token.split(' ')[1];  
-    //     // const privatekey =  process.env.privateKey;
-    //     // jwt.verify(token,privatekey, function(err, decoded){
-    //     //     if(err)
-    //     //     {
-    //     //         res.status(200).send({message:"invalid token"})
-    //     //     }else{
-    //     //     }      
-    //     //     res.header("Access-Control-Allow-Origin", "*");
-    //     //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    //     //     res.header("Access-Control-Allow-Methods", "PUT, GET,POST");
-    //     //     next();  
-    //     // })
-    //     // }
-    //    }else{
-    //        res.status(404).send({message:"invalid token"});
-    //    }
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Methods", "PUT, GET,POST");
+        next();
+      });
+      //    let Sqlquery = await tableNames.gen_token.findOne({ where: { gen_token: token,} });
+      //   console.log(Sqlquery);
+      //    if(Sqlquery != null)
+      //    {
+      //     //       if(!token){
+      //     //     res.status(404).send({message:"Token not found"})
+      //     // }else{
+      //     //        token = token.split(' ')[1];
+      //     // const privatekey =  process.env.privateKey;
+      //     // jwt.verify(token,privatekey, function(err, decoded){
+      //     //     if(err)
+      //     //     {
+      //     //         res.status(200).send({message:"invalid token"})
+      //     //     }else{
+      //     //     }
+      //     //     res.header("Access-Control-Allow-Origin", "*");
+      //     //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      //     //     res.header("Access-Control-Allow-Methods", "PUT, GET,POST");
+      //     //     next();
+      //     // })
+      //     // }
+      //    }else{
+      //        res.status(404).send({message:"invalid token"});
+      //    }
     }
-    } catch (error) {
-        res.status(404).send({message:"Token has not been provided"});
-    }
-
+  } catch (error) {
+    res.status(404).send({ message: "Token has not been provided" });
+  }
 }
 module.exports = {
-    authJWT,
-}
+  authJWT,
+};
--- routes/brands/profile/index.js
+++ routes/brands/profile/index.js
@@ -6,7 +6,7 @@ const {
     updateBrandprofile
 } = require('../../../controllers/brands/profile/brand_user.controller');
 
-router.get("/get-brand-profile/:brand_id",(req, res)=> {
+router.get("/brand-profile/:brand_id",(req, res)=> {
     getProfile(req, res);
 });
 
