87e2e639fd208de82aeaefd8841f6ebe24c6ed48
harsh gupta <theharshgupta2020@gmail.com>
2023-07-20 13:32:28 +0530

    edit Address api

--- controllers/influencer/address/address.controller.js
+++ controllers/influencer/address/address.controller.js
@@ -37,7 +37,7 @@ async function add_address(req, res) {
 
 async function getAddress(req, res) {
   const influencer_id = req.params.influencer_id;
-  selectQuery = `SELECT * FROM ${tableNames.influencer_address} where influencer_id =${influencer_id}`;
+  selectQuery = `SELECT * FROM ${tableNames.influencer_address} where influencer_id =${influencer_id} and 	delete_flag = 0`;
   result = await sequelize.query(selectQuery, {
     type: sequelize.QueryTypes.SELECT,
   });
@@ -80,8 +80,39 @@ async function deleteAddress(req, res) {
     });
   }
 }
+
+async function editAddress(req, res) {
+  influencer_id = req.params.influencer_id;
+
+  influencer_address_id = req.body.influencer_address_id;
+
+  address = req.body.address;
+  country = req.body.country;
+  city_id = req.body.city_id;
+  state_id = req.body.state_id;
+  pin = req.body.pin;
+
+  const sqlQuery = `UPDATE ${tableNames.influencer_address} SET address= '${address}', country= '${country}', city_id= '${city_id}', state_id= '${state_id}', pin= '${pin}' WHERE influencer_id = ${influencer_id} and influencer_address_id = ${influencer_address_id}`;
+  // `UPDATE ${tableNames.influencer_address} SET address= "address" WHERE influencer_id =37 and influencer_address_id =7;`;
+  var result = await sequelize.query(sqlQuery, {
+    type: sequelize.QueryTypes.UPDATE,
+  });
+  if (result.length != 0) {
+    res.status(200).send({
+      status: 200,
+      message: " address updated",
+    });
+  } else {
+    res.status(404).send({
+      status: 404,
+      message: " address not updated",
+    });
+  }
+}
+
 module.exports = {
   add_address,
   getAddress,
   deleteAddress,
+  editAddress,
 };
--- routes/influencers/address/index.js
+++ routes/influencers/address/index.js
@@ -1,20 +1,22 @@
-
 const express = require("express");
 const router = express.Router();
 const {
-    add_address,
-    getAddress,
-    deleteAddress
-} = require('../../../controllers/influencer/address/address.controller');
+  add_address,
+  getAddress,
+  deleteAddress,
+  editAddress
+} = require("../../../controllers/influencer/address/address.controller");
 
-router.post("/add/:influencer_id",(req, res)=> {
-    add_address(req, res);
+router.post("/add/:influencer_id", (req, res) => {
+  add_address(req, res);
 });
-router.get("/get/:influencer_id",(req, res)=> {
-    getAddress(req, res);
+router.get("/get/:influencer_id", (req, res) => {
+  getAddress(req, res);
 });
-router.delete("/delete/:influencer_id",(req, res)=> {
-    deleteAddress(req, res);
+router.delete("/delete/:influencer_id", (req, res) => {
+  deleteAddress(req, res);
 });
-
-module.exports =  router;
+router.patch("/edit/:influencer_id", (req, res) => {
+  editAddress(req, res);
+});
+module.exports = router;
