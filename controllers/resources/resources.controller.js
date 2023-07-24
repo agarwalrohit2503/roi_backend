be27940625c3139cdfd747a5bd0b67f98446d5e1
harsh gupta <theharshgupta2020@gmail.com>
2023-07-24 22:26:39 +0530

    new

--- controllers/resources/resources.controller.js
+++ controllers/resources/resources.controller.js
@@ -28,26 +28,6 @@ async function getState(req, res) {
   }
 }
 
-// async function getState(req, res) {
-//   selectQuery = `SELECT * FROM ${tableNames.state}`;
-//   result = await sequelize.query(selectQuery, {
-//     type: sequelize.QueryTypes.SELECT,
-//   });
-
-//   if (result.length != 0) {
-//     res.status(200).send({
-//       status: 200,
-//       message: "Data found",
-//       data: result,
-//     });
-//   } else {
-//     res.status(404).send({
-//       status: 404,
-//       message: "INTERNAL ERROR",
-//     });
-//   }
-// }
-
 async function getCity(req, res) {
   state_id = req.query.state_id;
 
@@ -110,26 +90,6 @@ async function getContentNiche(req, res) {
     });
   }
 }
-
-// async function getContentNiche(req, res) {
-//   selectQuery = `SELECT * FROM ${tableNames.content_niche}`;
-//   result = await sequelize.query(selectQuery, {
-//     type: sequelize.QueryTypes.SELECT,
-//   });
-
-//   if (result.length != 0) {
-//     res.status(200).send({
-//       status: 200,
-//       message: "Content Niche data",
-//       data: result,
-//     });
-//   } else {
-//     res.status(404).send({
-//       status: 404,
-//       message: "INTERNAL ERROR",
-//     });
-//   }
-// }
 
 async function getBusinessList(req, res) {
 
