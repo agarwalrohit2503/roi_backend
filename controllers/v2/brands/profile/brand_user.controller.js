d30a952f52e63abcd59019733a996781207b9224
harsh gupta <theharshgupta2020@gmail.com>
2023-08-10 15:45:47 +0530

    brand  profile api

--- controllers/v2/brands/profile/brand_user.controller.js
+++ controllers/v2/brands/profile/brand_user.controller.js
@@ -93,7 +93,7 @@ async function updateBrandprofile(req, res) {
     {
       brand_logo: brand_logo,
       brand_type_id: brand_type_id,
-      Name: Name,
+      name: Name,
       email: email,
       number: number,
       pan_card: pan_card,
