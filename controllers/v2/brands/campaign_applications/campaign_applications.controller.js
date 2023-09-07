47322d031932fb40013c0bc8b4bcf257ed455c64
harsh gupta <theharshgupta2020@gmail.com>
2023-09-07 22:05:21 +0530

    campaign application api

--- controllers/v2/brands/campaign_applications/campaign_applications.controller.js
+++ controllers/v2/brands/campaign_applications/campaign_applications.controller.js
@@ -10,7 +10,13 @@ async function get(req, res) {
   var offset = req.query.offset;
   var application_status = req.query.application_status;
   var search_term = req.query.search_term;
-  
+
+  if (campaign_id == "" || campaign_id == null || campaign_id == 0) {
+    return res
+      .status(409)
+      .send({ status: 409, message: "campaign id is empty" });
+  }
+
   const findQuery = await tableNames.campaignApplication.findAll({
     attributes: ["campaign_applied_id", "campaign_id"],
 
@@ -54,152 +60,46 @@ async function get(req, res) {
 
   res.status(200).send({
     status: 200,
-
     message: findQuery != "" ? "Data found" : "Data not found",
     data: findQuery,
   });
 }
 
 async function update(req, res) {
-  var brand_id = req.params.brand_id;
-  var campaign_id = req.body.campaign_id;
+  var campaign_id = req.params.campaign_id;
 
-  var content_niche_id = req.body.content_niche_id;
-  var platform_id = req.body.platform_id;
-  var campaign_goal_id = req.body.campaign_goal_id;
-  var campaign_status_id = req.body.campaign_status_id;
-  var payment_status_id = req.body.payment_status_id;
-  var campaign_name = req.body.campaign_name;
-  var location = req.body.location;
-  var campaign_about = req.body.campaign_about;
-  var about_product = req.body.about_product;
-  var language = req.body.language;
-  var campaign_start_dt = req.body.campaign_start_dt;
-  var campaign_end_dt = req.body.campaign_end_dt;
-  var campaign_budget = req.body.campaign_budget;
-  var image_link = req.body.image_link;
-  var platform = req.body.platform;
-  var eligibility = req.body.eligibility;
+  var campaign_applied_id = req.query.campaign_applied_id;
+  var application_status = req.body.application_status;
 
-  var post = req.body.post;
-  var story = req.body.story;
-  var real = req.body.real;
-  var youtube = req.body.youtube;
-
-  if (image_link != "") {
-    image_link = await imageUpload(image_link);
+  if (campaign_id == "" || campaign_id == null || campaign_id == 0) {
+    return res
+      .status(409)
+      .send({ status: 409, message: "campaign id is empty" });
   }
-
   try {
-    let campaingEditParameters = {
-      campaign_status_id: campaign_status_id,
-      payment_status_id: payment_status_id,
-      campaign_goal_id: campaign_goal_id,
-      campaign_name: campaign_name,
-      location: location,
-      campaign_about: campaign_about,
-      about_product: about_product,
-      language: language,
-      campaign_start_dt: campaign_start_dt,
-      campaign_end_dt: campaign_end_dt,
-      campaign_budget: campaign_budget,
-      image_link: image_link,
-      platform: platform,
-      eligibility: eligibility,
+    let campaignApplicationParameters = {
+      application_status_id: application_status,
     };
-    var campaignEditedValue = await editParameterQuery(campaingEditParameters);
-
-    const updateQuery = await tableNames.Campaign.update(campaignEditedValue, {
-      where: {
-        campaign_id: campaign_id,
-        brand_id: brand_id,
-        campaign_delete: 0,
-      },
-    });
-
-    if (updateQuery[0] != "") {
-      let campaignContentNicheRespData = await Promise.all(
-        content_niche_id.map(async (item) => {
-          try {
-            let content_niche_info = {
-              campaign_id: campaign_id,
-              content_niche_id: item,
-            };
-            insertContentNicheQuery =
-              await tableNames.campaignContentNiche.create(content_niche_info);
-
-            return insertContentNicheQuery;
-          } catch (error) {
-            return { ...item, error };
-          }
-        })
-      );
-
-      console.log(campaignContentNicheRespData);
-      if (
-        campaignContentNicheRespData == "" ||
-        campaignContentNicheRespData == null
-      ) {
-        res.status(209).send({
-          status: 209,
-          message: "Campaing Content Niche not inserted",
-        });
-      }
-
-      let campaignPlatformRespData = await Promise.all(
-        platform_id.map(async (item) => {
-          try {
-            let campaign_campaignPlatform_info = {
-              campaign_id: campaign_id,
-              platform_id: item,
-            };
-            insertcampaignPlatformQuery =
-              await tableNames.campaignPlatform.create(
-                campaign_campaignPlatform_info
-              );
-
-            return insertcampaignPlatformQuery;
-          } catch (error) {
-            return { ...item, error };
-          }
-        })
-      );
-      if (
-        (campaignPlatformRespData == "") |
-        (campaignPlatformRespData == null)
-      ) {
-        res.status(209).send({
-          status: 209,
-          message: "Platform Not Inserted",
-        });
-      }
-
-      let DeliverablesEdit = {
-        post: post,
-        story: story,
-        real: real,
-        youtube: youtube,
-      };
-      var DeliverablesEditedData = await editParameterQuery(DeliverablesEdit);
-
-      const updateQuery = await tableNames.campaignDeliverables.update(
-        DeliverablesEditedData,
+    const campaignApplicationUpdateQuery =
+      await tableNames.campaignApplication.update(
+        campaignApplicationParameters,
         {
           where: {
             campaign_id: campaign_id,
+            campaign_applied_id: campaign_applied_id,
           },
         }
       );
 
-      //console.log(updateQuery);
+    if (campaignApplicationUpdateQuery[0] != "") {
       res.status(200).send({
         status: 200,
-        message: "Campaign edited",
+        message: "Application status is updated",
       });
     } else {
       res.status(409).send({
         status: 409,
-        message: "Campaign not edited",
+        message: "Application status is not updated",
       });
     }
   } catch (err) {
