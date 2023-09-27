e71d9e0aeaa91dfbf2dd0ae8d9c3398191439401
harsh gupta <theharshgupta2020@gmail.com>
2023-09-27 14:10:33 +0530

    test

--- utils/operator_aliases.js
+++ utils/operator_aliases.js
@@ -5,6 +5,14 @@ const operatorsAliases = {
   $eq: Op.eq,
   $or: Op.or,
   $like: Op.like,
+  $in: Op.in,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $and: Op.and,
+  $lte: Op.lte,
+  $between: Op.between,
+  $not: Op.not,
+  $ne: Op.ne,
 };
 
 module.exports = operatorsAliases;
