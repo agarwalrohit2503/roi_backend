67a3c32cb11517ff0c8d40e1c5ebf1f508cd417f
Setstate <setstatetechnologies@gmail.com>
2023-11-08 17:35:01 +0530

    fb user details inserted api done

--- controllers/v2/influencer/influencer_social_media/influencer_social_media.controller.js
+++ controllers/v2/influencer/influencer_social_media/influencer_social_media.controller.js
@@ -26,12 +26,40 @@ async function addInfluencerSocialMediaDetails(req, res) {
       // "/me?fields=id,name,birthday,age_range,posts{full_picture,permalink_url}"
       "/me?fields=id,name,birthday,age_range,link,about,gender,location,posts.limit(5){link,created_time},photos{link,images,created_time}"
     )
-    .then((response) => {
+    .then(async (response) => {
       // var name = response.data.name;
       // res.send(name);
 
+      console.log(response.data.id);
+      console.log(response.data.name);
+      console.log(response.data.birthday);
+      console.log(response.data.age_range.min);
+      console.log(response.data.link);
+      console.log(response.data.gender);
+      console.log(response.data.location.name);
+
       //JSON RESP BODY
-      success(res, "Influencer social", "Influencer social", response.data);
+
+      fbInfluencerDetails = {
+        influencer_id: influencer_id,
+        fb_user_id: response.data.id,
+        name: response.data.name,
+        birthday: response.data.birthday,
+        age_range: response.data.age_range.min,
+        profile_link: response.data.link,
+        gender: response.data.gender,
+        location: response.data.location.name,
+      };
+
+      const fbinfluencerinsertQuery =
+        await tableNames.influencerFacebook.create(fbInfluencerDetails);
+
+      success(
+        res,
+        "Influencer facebook details inserted",
+        "Influencer facebook details not inserted, please try again",
+        fbinfluencerinsertQuery
+      );
     })
     .catch((error) => {
       res.send("not found");
