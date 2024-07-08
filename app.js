aa4be0dc6093d6999624c201032067022247fc2c
theharshguptaflutter <theharshgupta2020@gmail.com>
2024-07-08 19:15:19 +0530

    cd

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { swaggerServe, swaggerSetup } = require("./config");
+// const { swaggerServe, swaggerSetup } = require("./config");
 var cors = require("cors");
 const path = require("path");
 
@@ -32,7 +32,7 @@ app.listen(port, () => {
   console.log(`server is running at ${port}`);
 });
 
-app.use("/api-docs", swaggerServe, swaggerSetup);
+// app.use("/api-docs", swaggerServe, swaggerSetup);
 app.use("/v1", v1);
 app.use("/v2", v2);
 
